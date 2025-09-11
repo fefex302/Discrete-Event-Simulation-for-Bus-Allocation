92b94080c0c212a60e7eb3c326f53353ea6f2323
fefex302 <federico806@live.it>
2025-09-11 10:19:26 +0200

    Added comments

--- bus_simulation/simulation_core.py
+++ bus_simulation/simulation_core.py
@@ -7,6 +7,7 @@ class Bus:
     def __init__(self, env, name, capacity):
         self.env = env
         self.name = name
+        self.travels = 0  # Counter for the number of trips made
         self.capacity = capacity # Maximum number of passengers this bus can carry
         self.passengers_on_board = 0 # Counter for passengers currently on board
         self.traveling = False # Indicates if the bus is currently traveling
@@ -22,16 +23,17 @@ class Participant:
 
 # Used to store the configuration of the simulation
 class Configuration:
-    def __init__(self, num_navette, smart_driver, smart_time, smart_percentage, max_passengers):
+    def __init__(self, num_navette, smart_driver, smart_time, smart_percentage, smart_last_boarding_time, max_passengers):
         self.num_navette = num_navette
         self.smart_driver = smart_driver # Indicates if the bus driver is smart
         self.smart_time = smart_time # Time the bus can wait before deciding to leave
         self.smart_percentage = smart_percentage # Percentage of bus capacity that must be filled before the bus departs
+        self.smart_last_boarding_time = smart_last_boarding_time # Maximum time since the last boarding before the bus decides to leave
         self.max_passengers = max_passengers  # Maximum number of passengers for this configuration
         self.history = []  # List to keep track of passengers and their boarding and disembarking times
 
 # Function to generate participants (people to be boarded) in the simulation
-def participant_generator(env, passenger_queue, history, tempo_massimo_simulazione, MAX_PASSENGERS, PEAK_HOUR, BASE_LAMBDA, PEAK_FACTOR):
+def participant_generator(env, passenger_queue, history, tempo_massimo_simulazione, MAX_PASSENGERS, PEAK_HOUR, BASE_LAMBDA, PEAK_FACTOR, verbose = False):
     i = 0
     while len(history) < MAX_PASSENGERS:  # Limit the number of clients to MAX_PASSENGERS
         now = int(env.now)
@@ -43,7 +45,8 @@ def participant_generator(env, passenger_queue, history, tempo_massimo_simulazio
         # Generate a new participant based on the adjusted arrival rate
         yield env.timeout(random.expovariate(new_lambda))
         i += 1
-        print(f'{env.now:.2f}: Participant {i} arrives and joins general queue.')
+        if verbose:
+            print(f'{env.now:.2f}: Participant {i} arrives and joins general queue.')
 
         # Participants join a general queue, not looking for a specific bus right away
         passenger = Participant(env, i, env.now)
@@ -58,14 +61,21 @@ def participant_generator(env, passenger_queue, history, tempo_massimo_simulazio
 #     print(f'{env.now:.2f}: passenger {passenger.name} has boarded {bus_destination.name}.')
 #     # The client's action ends here for this simulation, but could include the journey and disembarkation
 
-def bus_process(env, bus_obj, bus_attivi_per_imbarco, passenger_queue, SMART_DRIVER, SMART_TIME, SMART_PERCENTAGE, TRAVEL_TIME):
+def bus_process(env, bus_obj, bus_attivi_per_imbarco, passenger_queue, SMART_DRIVER, SMART_TIME, SMART_PERCENTAGE, TRAVEL_TIME, HYBRID = False, HYBRID_TIME=125, verbose = False, TIME_FROM_LAST_BOARDING=2):
     # Bus cycle
+    smart_driver = SMART_DRIVER  # Local copy of SMART_DRIVER to allow dynamic changes
     while True:
         # The bus tries to put itself in the active boarding queue
         yield bus_attivi_per_imbarco.put(bus_obj)
+        if HYBRID:
+            if env.now >= HYBRID_TIME:
+                smart_driver = False
+                if verbose:
+                    print(f'{env.now:.2f}: {bus_obj.name} switches to non-smart driving.')    
 
         # After being put in the queue, the bus can board passengers
-        print(f'\n{env.now:.2f}: {bus_obj.name} is at the stop and active for boarding.')
+        if verbose:
+            print(f'\n{env.now:.2f}: {bus_obj.name} is at the stop and active for boarding.')
 
         bus_obj.passengers_on_board = 0 # Counter for passengers on board
         current_passengers = [] # A list to keep track of current passengers boarding this bus
@@ -73,16 +83,18 @@ def bus_process(env, bus_obj, bus_attivi_per_imbarco, passenger_queue, SMART_DRI
         time_from_last_boarding = 0  # Time since the last boarding
         time_of_last_boarding = env.now # Time of the last boarding action
 
-        # The bus will board passengers until it is full or the SMART_DRIVER conditions are met
-        while bus_obj.passengers_on_board < bus_obj.capacity and (not SMART_DRIVER or (env.now - time_idle <= SMART_TIME
-                                                                                               and bus_obj.passengers_on_board <= bus_obj.capacity * SMART_PERCENTAGE
-                                                                                               and time_from_last_boarding < 2)):
+        # # The bus will board passengers until it is full or the SMART_DRIVER conditions are met
+        while bus_obj.passengers_on_board < bus_obj.capacity and (not smart_driver or (env.now - time_idle <= SMART_TIME and bus_obj.passengers_on_board <= bus_obj.capacity * SMART_PERCENTAGE)
+                                                                                     or (env.now - time_idle <= SMART_TIME and time_from_last_boarding < TIME_FROM_LAST_BOARDING)):
+        # while bus_obj.passengers_on_board < bus_obj.capacity and (not smart_driver or (env.now - time_idle <= SMART_TIME
+        #                                                                                        and bus_obj.passengers_on_board <= bus_obj.capacity * SMART_PERCENTAGE
+        #                                                                                        and time_from_last_boarding < TIME_FROM_LAST_BOARDING)):
             # Wait for a passenger to be available in the queue
             passenger = yield passenger_queue.get()
             current_passengers.append(passenger)
 
             # update the frequency of boarding
-            if SMART_DRIVER:
+            if smart_driver:
                 time_from_last_boarding = env.now - time_of_last_boarding
                 time_of_last_boarding = env.now
 
@@ -90,11 +102,14 @@ def bus_process(env, bus_obj, bus_attivi_per_imbarco, passenger_queue, SMART_DRI
             #env.process(client(env, passenger, bus_obj))
             passenger.time_boarded = env.now  # Record the time the passenger boarded the bus
             bus_obj.passengers_on_board += 1
-                
-            print(f'{env.now:.2f}: {bus_obj.name}: boarded passenger {passenger.name}. Total: {bus_obj.passengers_on_board}/{bus_obj.capacity}.')
 
-        print(f'{env.now:.2f}: {bus_obj.name} is full ({bus_obj.passengers_on_board} passengers). now departing!')
-        
+            if verbose:
+                print(f'{env.now:.2f}: {bus_obj.name}: boarded passenger {passenger.name}. Total: {bus_obj.passengers_on_board}/{bus_obj.capacity}.')
+
+        bus_obj.travels += 1
+        if verbose:
+            print(f'{env.now:.2f}: {bus_obj.name} is full ({bus_obj.passengers_on_board} passengers). now departing!')
+
         for passenger in current_passengers:
             passenger.time_departure = env.now
 
@@ -107,6 +122,7 @@ def bus_process(env, bus_obj, bus_attivi_per_imbarco, passenger_queue, SMART_DRI
         yield env.timeout(TRAVEL_TIME)
         bus_obj.traveling = False
 
-        print(f'{env.now:.2f}: {bus_obj.name} has arrived at destination and is unloading passengers. Now returning empty.')
+        if verbose:
+            print(f'{env.now:.2f}: {bus_obj.name} has arrived at destination and is unloading passengers. Now returning empty.')
         bus_obj.passengers_on_board = 0 # Unload passengers
         # The bus is now available for the next cycle (will return to boarding queue)
